617d75190b12f5684225a47e2c65d47335edf8b5
ier <raman.karseka@gmail.com>
2022-10-16 12:12:45 +0300

    hw1

--- hw/hw1/src/index.ts
+++ hw/hw1/src/index.ts
@@ -1,4 +1,5 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
+import bodyParser from "body-parser"
 
 // enum Resolutions {
 //     P144 = 'P144',
@@ -11,97 +12,261 @@ import express, {Request, Response} from 'express'
 //     P2160 = 'P2160'
 // }
 
-type Resolution =
-    'P144'
-    | 'P240'
-    | 'P360'
-    | 'P480'
-    | 'P720'
-    | 'P1080'
-    | 'P1440'
-    | 'P2160'
+type Resolution = 'P144' | 'P240' | 'P360' | 'P480' | 'P720' | 'P1080' | 'P1440' | 'P2160'
 
 interface IVideo {
-    id: number,
-    title: string,
-    author: string,
-    canBeDownloaded: boolean,
-    minAgeRestriction: number | null,
-    createdAt: string,
-    publicationDate: string,
-    availableResolutions: string[]
+  id: number,
+  title: string,
+  author: string,
+  canBeDownloaded: boolean,
+  minAgeRestriction: number | null,
+  createdAt: string,
+  publicationDate: string,
+  availableResolutions: string[]
 }
+
+const resolutions = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
+
+const videos: IVideo[] = [
+  {
+    'id': 0,
+    'title': 'string',
+    'author': 'string',
+    'canBeDownloaded': true,
+    'minAgeRestriction': null,
+    'createdAt': '2023-03-09T17:27:25.966Z',
+    'publicationDate': '2023-03-09T17:27:25.966Z',
+    'availableResolutions': [
+      'P144'
+    ]
+  }, {
+    'id': 1,
+    'title': 'string',
+    'author': 'string',
+    'canBeDownloaded': true,
+    'minAgeRestriction': null,
+    'createdAt': '2023-03-09T17:27:25.966Z',
+    'publicationDate': '2023-03-09T17:27:25.966Z',
+    'availableResolutions': [
+      'P144'
+    ]
+  }
+]
+
+const requiredFields = [ 'title', 'author' ]
+
+const checkAvailableResolutions = (arr: string[]) => arr.every(r => resolutions.includes(r))
 
 const app = express()
 const port = 3000
-const videos: IVideo[] = [
-    {
-        'id': 0,
-        'title': 'string',
-        'author': 'string',
-        'canBeDownloaded': true,
-        'minAgeRestriction': null,
-        'createdAt': '2023-03-09T17:27:25.966Z',
-        'publicationDate': '2023-03-09T17:27:25.966Z',
-        'availableResolutions': [
-            'P144'
-        ]
-    }
-]
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
 app.delete('/ht_01/api/testing/all-data', (req: Request, res: Response) => {
-    videos.length = 0
-    res.send(204)
+  videos.length = 0
+  res.sendStatus(204)
 })
 
 app.get('/hometask_01/api/videos', (req: Request, res: Response) => {
-    res.send(videos)
+  res.send(videos)
 })
 
 app.post('/hometask_01/api/videos', (req: Request, res: Response) => {
-    const newVideo = {
-        id: +(new Date()),
-        title: req.body.title || '',
-        author: req.body.author || '',
-        canBeDownloaded: !!req.body.canBeDownloaded,
-        minAgeRestriction: req.body.minAgeRestriction || 21,
-        createdAt: '' + new Date(),
-        publicationDate: req.body.publicationDate || new Date(),
-        availableResolutions: req.body.availableResolutions
+  const title = req.body.title
+
+  if (!title || title.length < 1 || title.length > 40) {
+    res.status(400).send({
+      "errorsMessages": [
+        {
+          "message": "title length must be between 1 and 40",
+          "field": "title"
+        }
+      ]
+    })
+    return
+  }
+
+  const author = req.body.author
+
+  if (!author || author.length < 1 || req.body.author.length > 20) {
+    res.status(400).send({
+      "errorsMessages": [
+        {
+          "message": "author length must be between 1 and 20",
+          "field": "author"
+        }
+      ]
+    })
+    return
+  }
+
+  const availableResolutions: string[] = req.body.availableResolutions
+
+  const resolutionsError = {
+    "errorsMessages": [
+      {
+        "message": "resolutions format mistake",
+        "field": "availableResolutions"
+      }
+    ]
+  }
+  if (!Array.isArray(availableResolutions)) {
+    res.status(400).send(resolutionsError)
+    return
+  }
+
+  if (availableResolutions.length) {
+    if (!checkAvailableResolutions(availableResolutions)) {
+      res.status(400).send(resolutionsError)
+      return
     }
-    videos.push(newVideo)
-    res.status(201).send(newVideo)
+  } else {
+    availableResolutions.push('P144')
+  }
+
+  const newVideo = {
+    id: +(new Date()),
+    title,
+    author,
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date().toISOString(),
+    availableResolutions
+  }
+  videos.push(newVideo)
+  res.status(201).send(newVideo)
 })
 
 app.get('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
-    const id = +req.params.id
-    const video = videos.find(v => v.id === id)
-    if (video) {
-        res.send(video)
-    } else {
-        res.send(404)
-    }
+  const id = +req.params.id
+  const video = videos.find(v => v.id === id)
+  if (video) {
+    res.send(video)
+  } else {
+    res.send(404)
+  }
 })
 
 app.put('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
-    const id = +req.params.id
-    const video = videos.find(v => v.id === id)
-    if (video) {
-        res.send(video)
+  const id = +req.params.id
+  const index = videos.findIndex(v => v.id === id)
+  if (index === -1) {
+    res.send(401)
+    return
+  }
+  const video = videos[index]
+
+  const title = req.body.title
+
+  if (!title || title.length < 1 || title.length > 40) {
+    res.status(400).send({
+      "errorsMessages": [
+        {
+          "message": "title length must be between 1 and 40",
+          "field": "title"
+        }
+      ]
+    })
+    return
+  }
+
+  video.title = title
+
+  const author = req.body.author
+
+  if (!author || author.length < 1 || req.body.author.length > 20) {
+    res.status(400).send({
+      "errorsMessages": [
+        {
+          "message": "author length must be between 1 and 20",
+          "field": "author"
+        }
+      ]
+    })
+    return
+  }
+
+  video.author = author
+
+  const availableResolutions: string[] = req.body.availableResolutions
+
+  if (availableResolutions) {
+    if (Array.isArray(availableResolutions) && checkAvailableResolutions(availableResolutions)) {
+      video.availableResolutions = availableResolutions
     } else {
-        res.send(404)
+      res.status(400).send({
+        "errorsMessages": [
+          {
+            "message": "resolutions format mistake",
+            "field": "availableResolutions"
+          }
+        ]
+      })
+      return
     }
+  }
+
+  const canBeDownloaded = req.body.canBeDownloaded
+  if (canBeDownloaded !== undefined) {
+    if (typeof canBeDownloaded == "boolean") {
+      video.canBeDownloaded = canBeDownloaded
+    } else {
+      res.status(400).send({
+        "errorsMessages": [
+          {
+            "message": "canBeDownloaded format mistake",
+            "field": "canBeDownloaded"
+          }
+        ]
+      })
+      return
+    }
+  }
+
+  const minAgeRestriction = req.body.minAgeRestriction
+  if (minAgeRestriction !== undefined) {
+    if (minAgeRestriction === null || (Number.isInteger(minAgeRestriction) && minAgeRestriction > 1 && minAgeRestriction < 18)) {
+      video.minAgeRestriction = minAgeRestriction
+    } else {
+      res.status(400).send({
+        "errorsMessages": [
+          {
+            "message": "minAgeRestriction format mistake",
+            "field": "minAgeRestriction"
+          }
+        ]
+      })
+      return
+    }
+  }
+
+  const publicationDate = req.body.publicationDate
+  if (minAgeRestriction !== undefined) {
+    video.publicationDate = publicationDate
+  }
+  videos[index] = video
+
+  res.send(204)
+
 })
 
-app.put('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
-    const id = +req.params.id
-    const video = videos.find(v => v.id === id)
-    if (video) {
-        res.send(video)
-    } else {
-        res.send(404)
-    }
+app.delete('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
+  const id = +req.params.id
+  const videoIndex = videos.findIndex(v => v.id === id)
+  if (videoIndex !== -1) {
+    videos.splice(videoIndex, 1)
+    res.status(204)
+  } else {
+    res.send(404)
+  }
 })
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${ port }`)
 })
