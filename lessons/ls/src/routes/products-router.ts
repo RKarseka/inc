ef2d11247b71d45eac49a1187349cf5b5bba249d
ier <raman.karseka@gmail.com>
2022-10-21 12:13:00 +0300

    [ls1-2-7]

--- lessons/ls/src/routes/products-router.ts
+++ lessons/ls/src/routes/products-router.ts
@@ -1,31 +1,38 @@
 import { Request, Response, Router } from "express";
 import { productsRepository } from "../repositories/products-repository";
+import { body, validationResult } from "express-validator";
+import { inputValidationMiddleware } from "../middlewares/input-validation-midleware";
 
 
 export const productsRouter = Router({})
+
+const titleValidation = body('title').trim().isLength({min: 3, max: 10}).withMessage('error message')
 
 productsRouter.get('/', (req: Request, res: Response) => {
   const foundProducts = productsRepository.findProducts(req.query.title?.toString())
   res.send(foundProducts)
 })
 
-productsRouter.post('/', (req: Request, res: Response) => {
-  res.status(201).send(productsRepository.createProduct(req.body.title))
-})
+productsRouter.post('/', titleValidation, inputValidationMiddleware,
+  (req: Request, res: Response) => {
+    res.status(201).send(productsRepository.createProduct(req.body.title))
+  })
 
-productsRouter.get('/:id', (req: Request, res: Response) => {
-  const product = productsRepository.getProductById(+req.params.id)
-  if (product) {
-    res.send(product)
-  } else {
-    res.send(404)
-  }
-})
+productsRouter.get('/:id',
+  (req: Request, res: Response) => {
+    const product = productsRepository.getProductById(+req.params.id)
+    if (product) {
+      res.send(product)
+    } else {
+      res.send(404)
+    }
+  })
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
-  const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
-  res.send(isUpdated ? productsRepository.getProductById(+req.params.id) : 404)
-})
+productsRouter.put('/:id', titleValidation, inputValidationMiddleware,
+  (req: Request, res: Response) => {
+    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+    res.send(isUpdated ? productsRepository.getProductById(+req.params.id) : 404)
+  })
 
 productsRouter.delete('/:id', (req: Request, res: Response) => {
   res.sendStatus(productsRepository.deleteProduct(+req.params.id) ? 204 : 404)
