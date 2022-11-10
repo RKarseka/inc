d653171dab7361def892c34e4f3113179b692dbf
raman <raman.karseka@gmail.com>
2022-11-10 12:14:09 +0300

    hw6-3.3 Added jwt validation to comments

--- hw/hw1/src/01.routers/comments-router.ts
+++ hw/hw1/src/01.routers/comments-router.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { commentsService } from "../02.domain/comments-service";
 import { vAuth, vEComment } from "../validators/validators";
 import { authValidationMiddleware, inputValidationMiddleware } from "../middlewares/input-validation-midleware";
+import {authMiddleware} from "../middlewares/auth-middleware";
 
 
 export const commentsRouter = Router({})
@@ -10,10 +11,10 @@ commentsRouter.get('/:id', async (req: Request, res: Response) => {
     res.send(await commentsService.getOneComment(req.params.id))
   }
 )
-commentsRouter.put('/:id', vEComment, vAuth, authValidationMiddleware, inputValidationMiddleware,
+commentsRouter.put('/:id', vEComment, authMiddleware, inputValidationMiddleware,
   async (req: Request, res: Response) => {
     res.sendStatus(await commentsService.editOwnComment(req.params.id, req.body))
   })
-commentsRouter.delete('/:id', vAuth, authValidationMiddleware, async (req: Request, res: Response) => {
+commentsRouter.delete('/:id', authMiddleware, async (req: Request, res: Response) => {
   res.sendStatus(await commentsService.delOwnComment(req.params.id))
 })
