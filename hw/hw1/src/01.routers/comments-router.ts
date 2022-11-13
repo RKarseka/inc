96ef2552396dadd1cd7d45bd69d282c6a48013ca
raman <raman.karseka@gmail.com>
2022-11-13 12:14:09 +0300

    hw6-5 Fixed get one comment

--- hw/hw1/src/01.routers/comments-router.ts
+++ hw/hw1/src/01.routers/comments-router.ts
@@ -1,14 +1,19 @@
-import { Request, Response, Router } from "express";
-import { commentsService } from "../02.domain/comments-service";
-import { vAuth, vEComment } from "../validators/validators";
-import { authValidationMiddleware, inputValidationMiddleware } from "../middlewares/input-validation-midleware";
+import {Request, Response, Router} from "express";
+import {commentsService} from "../02.domain/comments-service";
+import {vAuth, vEComment} from "../validators/validators";
+import {authValidationMiddleware, inputValidationMiddleware} from "../middlewares/input-validation-midleware";
 import {authMiddleware} from "../middlewares/auth-middleware";
 
 
 export const commentsRouter = Router({})
 
 commentsRouter.get('/:id', async (req: Request, res: Response) => {
-    res.send(await commentsService.getOneComment(req.params.id))
+    const comment = await commentsService.getOneComment(req.params.id)
+    if (comment) {
+      res.send(comment)
+    } else {
+      res.sendStatus(404)
+    }
   }
 )
 commentsRouter.put('/:id', vEComment, authMiddleware, inputValidationMiddleware,
