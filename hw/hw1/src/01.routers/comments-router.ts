b48f2f43dbbaa1dbe2198d32b0f4bb1811952a0b
raman <raman.karseka@gmail.com>
2023-01-12 12:14:09 +0300

    hw 11-4 s3-3 commants likes

--- hw/hw1/src/01.routers/comments-router.ts
+++ hw/hw1/src/01.routers/comments-router.ts
@@ -1,19 +1,19 @@
-import { Request, Response, Router } from 'express'
-import { commentsService } from '../02.domain/comments-service'
-import { vEComment } from '../validators/validators'
+import {Request, Response, Router} from 'express'
+import {commentsService} from '../02.domain/comments-service'
+import {vEComment} from '../validators/validators'
 import {authValidationMiddleware, inputValidationMiddleware} from '../middlewares/input-validation-midleware'
-import { authMiddleware } from '../middlewares/auth-middleware'
+import {authMiddleware} from '../middlewares/auth-middleware'
 
 export const commentsRouter = Router({})
 
-commentsRouter.get('/:id', async (req: Request, res: Response) => {
-  const comment = await commentsService.getOneComment(req.params.id, req.userId || '')
-  if (comment != null) {
-    res.send(comment)
-  } else {
-    res.sendStatus(404)
+commentsRouter.get('/:id', authMiddleware, async (req: Request, res: Response) => {
+    const comment = await commentsService.getOneComment(req.params.id, req.userId || '')
+    if (comment != null) {
+      res.send(comment)
+    } else {
+      res.sendStatus(404)
+    }
   }
-}
 )
 commentsRouter.put('/:id', commentsService.checkCommentPresent, vEComment, authMiddleware, inputValidationMiddleware,
   async (req: Request, res: Response) => {
