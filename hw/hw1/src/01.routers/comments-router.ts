0134450315ad53697f13062823f37b478f4ab813
raman <raman.karseka@gmail.com>
2023-01-28 12:14:09 +0300

    hw 11-20 s3-3 commants likes

--- hw/hw1/src/01.routers/comments-router.ts
+++ hw/hw1/src/01.routers/comments-router.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from 'express'
 import {commentsService} from '../02.domain/comments-service'
-import {vEComment} from '../validators/validators'
+import {vEComment, vLikeComment} from '../validators/validators'
 import {authValidationMiddleware, inputValidationMiddleware} from '../middlewares/input-validation-midleware'
 import {authMiddleware, checkAuthorizationMiddleware} from '../middlewares/auth-middleware'
 import {jwtService} from "../-application/jwt-service";
@@ -8,7 +8,7 @@ import {jwtService} from "../-application/jwt-service";
 export const commentsRouter = Router({})
 
 commentsRouter.get('/:id', checkAuthorizationMiddleware, async (req: Request, res: Response) => {
-  console.log('const req.user = ', req.user)
+    console.log('const req.user = ', req.user)
     const comment = await commentsService.getOneComment(req.params.id, req.user.userId)
     if (comment != null) {
       res.send(comment)
@@ -28,7 +28,7 @@ commentsRouter.delete('/:id', commentsService.checkCommentPresent, authMiddlewar
 })
 
 
-commentsRouter.put('/:id/like-status', commentsService.checkCommentPresent, authMiddleware, inputValidationMiddleware,
+commentsRouter.put('/:id/like-status', commentsService.checkCommentPresent, vLikeComment, authMiddleware, inputValidationMiddleware,
   async (req: Request, res: Response) => {
     res.sendStatus(await commentsService.setCommentLike(req.params.id, req.body, req.user.userId))
   })
